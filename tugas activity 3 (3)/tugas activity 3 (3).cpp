135ac5b0569d8c4093da9fc75dabe4e8d291b57b
firyalshafa <shafafiryal782@gmail.com>
2024-03-05 17:57:47 +0700

    Add project files.

---
+++ tugas activity 3 (3)/tugas activity 3 (3).cpp
@@ -0,0 +1,27 @@
+// tugas activity 3 (3).cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using  namespace std;
+
+
+int main()
+{
+    int bilangan;
+    srand(time(0));
+    bilangan = rand() % 10;
+
+    cout << "Nilai awal : " << bilangan << endl;
+
+    if (bilangan >= 5)
+    {
+        bilangan = 2 * bilangan;
+    }
+    else
+    {
+        bilangan = 4 * bilangan;
+    }
+    cout << "nilai sekarang = " << bilangan << endl;
+}
+
+
---
+++ tugas activity 3/tugas activity 3.cpp
@@ -0,0 +1,23 @@
+// tugas activity 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int bilangan;
+    srand(time(0));
+    bilangan = rand() % 10;
+
+    cout << "nilai awal : " << bilangan << endl;
+
+    if (bilangan >= 5)
+    {
+        bilangan = 2 * bilangan;
+
+    }
+
+    cout << "bilangan sekarang =" << bilangan << endl;
+}
+
